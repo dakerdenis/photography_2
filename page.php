f8a4b0e205a59220b6e3f51dfc961d74975611d0
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-05-30 10:37:47 +0400

    фввув

--- page.php
+++ page.php
@@ -2,206 +2,158 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Fahri Bagirov</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Fahri Bagirov</title>
 
-  <!--ico--->
-  <link rel="shortcut icon" href="/imgs/ico.png" type="image/x-icon" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <!-- Link Swiper's CSS -->
+    <!--ico--->
+    <link rel="shortcut icon" href="/imgs/ico.png" type="image/x-icon" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Link Swiper's CSS -->
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
-  <link rel="stylesheet" href="./style/swiper.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="./style/swiper.css" />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-  <!---main styles-->
-  <link rel="stylesheet" href="./style/contact.css" />
-  <link rel="stylesheet" href="./style/style.css" />
-  <link rel="stylesheet" href="./style/burger.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <!---main styles-->
+    <link rel="stylesheet" href="./style/contact.css" />
+    <link rel="stylesheet" href="./style/style.css" />
+    <link rel="stylesheet" href="./style/burger.css" />
 </head>
 
 <body>
-  <!-- main container----->
-  <div class="main_wrapper">
-    <!-------->
-    <div class="header__container header__container_services">
-      <div class="header__wrapper">
-        <!---header img--->
-        <div class="header__image">
-          <a href="./index.html">
-            Fahri Bagirov
-          </a>
-        </div>
-        <!----header navigation-->
-        <div class="header__nav_wrapper ">
-          <div class="header__nav__element header__nav__element_services">
-            <a href="#element1">Галлерея</a>
-          </div>
-          <div class="header__nav__element header__nav__element_services">
-            <a href="./about.html">О нас</a>
-          </div>
-
-          <div class="header__nav__element header__nav__element_services" >
-            <a href="./services.html">Услуги</a>
-          </div>
-          <div class="header__nav__element header__nav__element_services">
-            <a href="#element4">Контакты</a>
-          </div>
-          <!---language---->
-          <div class="header__nav__element header__nav__element_language header__nav__element_language_services">
-            <div class="dropdown-container">
-              <label for="openDropdown" class="dropdown">
-                RU
-                <i class="fa fa-caret-down" aria-hidden="true"></i>
-              </label>
-
-              <input type="checkbox" id="openDropdown" hidden />
-
-              <div class="dropdown-menu">
-                <span>AZ</span>
-                <span>EN</span>
-                <span>RU</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!----header mobile burger-->
-        <div class="mobile__burger_block">
-          <div class="burger-menu">
-            <a href="" class="burger-menu_button">
-              <spun class="burger-menu_lines"></spun>
-            </a>
-            <nav class="burger-menu_nav">
-              <div class="burger_menu__img">
-                <img src="./imgs/Logo.svg" alt="" />
-              </div>
-              <a href="#element1" class="burger-menu_link burger-menu_link_active">Галлерея</a>
-              <a href="#element2" class="burger-menu_link">О нас</a>
-              <a href="#element3" class="burger-menu_link">Услуги</a>
-              <a href="#element4" class="burger-menu_link">Контакты</a>
-              <div class="burger_menu__all_rights">
-                <p>Copyright © 1997—2008 «Fintrend NBCO»</p>
-              </div>
-            </nav>
-            <div class="burger-menu_overlay"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="main__content">
-        <div class="page__page__container">
-
-        <div class="page__page__promo__content">
-            <div class="page__page__img__block">
-                <img src="" alt="">
-            </div>
-            <div class="page__page__text">
-                <p></p>
-            </div>
-        </div>
-        <div class="gallery__content__container">
-                    <div class="gallery__photos__wrapper">
-                        <a
-                          href="./imgs/content/1.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/1.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/2.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/2.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/3.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/3.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/4.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/4.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/5.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/5.jpg" alt="" srcset="" />
-                        </a>
-        
-                        <a
-                          href="./imgs/content/6.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/6.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/7.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/7.jpg" alt="" srcset="" />
-                        </a>
-        
-                        <a
-                          href="./imgs/content/8.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/8.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                        href="./imgs/content/9.jpg"
-                        data-fancybox="gallery"
-                        data-caption="Optional caption"
-                      >
-                        <img src="./imgs/content/9.jpg" alt="" srcset="" />
-                      </a>
-                      <a
-                      href="./imgs/content/10.jpg"
-                      data-fancybox="gallery"
-                      data-caption="Optional caption"
-                    >
-                      <img src="./imgs/content/10.jpg" alt="" srcset="" />
+    <!-- main container----->
+    <div class="main_wrapper">
+        <!-------->
+        <div class="header__container header__container_services">
+            <div class="header__wrapper">
+                <!---header img--->
+                <div class="header__image">
+                    <a href="./index.html">
+                        Fahri Bagirov
                     </a>
-                    <a
-                    href="./imgs/content/11.jpg"
-                    data-fancybox="gallery"
-                    data-caption="Optional caption"
-                  >
-                    <img src="./imgs/content/11.jpg" alt="" srcset="" />
-                  </a>
-                  <a
-                  href="./imgs/content/12.jpg"
-                  data-fancybox="gallery"
-                  data-caption="Optional caption"
-                >
-                  <img src="./imgs/content/12.jpg" alt="" srcset="" />
-                </a>
-                      </div>
                 </div>
-        
-        </div>
-        <?php
+                <!----header navigation-->
+                <div class="header__nav_wrapper ">
+                    <div class="header__nav__element header__nav__element_services">
+                        <a href="#element1">Галлерея</a>
+                    </div>
+                    <div class="header__nav__element header__nav__element_services">
+                        <a href="./about.html">О нас</a>
+                    </div>
 
-            if(isset($_GET['foto'])){
+                    <div class="header__nav__element header__nav__element_services">
+                        <a href="./services.html">Услуги</a>
+                    </div>
+                    <div class="header__nav__element header__nav__element_services">
+                        <a href="#element4">Контакты</a>
+                    </div>
+                    <!---language---->
+                    <div class="header__nav__element header__nav__element_language header__nav__element_language_services">
+                        <div class="dropdown-container">
+                            <label for="openDropdown" class="dropdown">
+                                RU
+                                <i class="fa fa-caret-down" aria-hidden="true"></i>
+                            </label>
+
+                            <input type="checkbox" id="openDropdown" hidden />
+
+                            <div class="dropdown-menu">
+                                <span>AZ</span>
+                                <span>EN</span>
+                                <span>RU</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!----header mobile burger-->
+                <div class="mobile__burger_block">
+                    <div class="burger-menu">
+                        <a href="" class="burger-menu_button">
+                            <spun class="burger-menu_lines"></spun>
+                        </a>
+                        <nav class="burger-menu_nav">
+                            <div class="burger_menu__img">
+                                <img src="./imgs/Logo.svg" alt="" />
+                            </div>
+                            <a href="#element1" class="burger-menu_link burger-menu_link_active">Галлерея</a>
+                            <a href="#element2" class="burger-menu_link">О нас</a>
+                            <a href="#element3" class="burger-menu_link">Услуги</a>
+                            <a href="#element4" class="burger-menu_link">Контакты</a>
+                            <div class="burger_menu__all_rights">
+                                <p>Copyright © 1997—2008 «Fintrend NBCO»</p>
+                            </div>
+                        </nav>
+                        <div class="burger-menu_overlay"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main__content">
+            <div class="page__page__container">
+
+                <div class="page__page__promo__content">
+                    <div class="page__page__img__block">
+                        <img src="" alt="">
+                    </div>
+                    <div class="page__page__text">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="gallery__content__container">
+                    <div class="gallery__photos__wrapper">
+                        <a href="./imgs/content/1.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/1.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/2.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/2.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/3.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/3.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/4.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/4.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/5.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/5.jpg" alt="" srcset="" />
+                        </a>
+
+                        <a href="./imgs/content/6.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/6.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/7.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/7.jpg" alt="" srcset="" />
+                        </a>
+
+                        <a href="./imgs/content/8.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/8.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/9.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/9.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/10.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/10.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/11.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/11.jpg" alt="" srcset="" />
+                        </a>
+                        <a href="./imgs/content/12.jpg" data-fancybox="gallery" data-caption="Optional caption">
+                            <img src="./imgs/content/12.jpg" alt="" srcset="" />
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <?php
+
+            if (isset($_GET['foto'])) {
                 echo $_GET['foto'];
             }
-        
-        ?>
-    </div>
-    <script src="./js/script.js"></script>          
+
+            ?>
+        </div>
+        <script src="./js/script.js"></script>
 </body>
 
 </html>
