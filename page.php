98cb200989076832fef4435b1b5286db99d363eb
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-05-31 10:42:50 +0400

    added

--- page.php
+++ page.php
@@ -1,90 +1,90 @@
 <?php
-    $page_name;
-    $gallery_name;
+$page_name;
+$gallery_name;
 
 
-    if(isset($_GET['foto'])){
-        $foto = $_GET['foto'];
-        if($_GET['lang']=='ru'){
-            switch($foto){
-                case "family":
-                    $page_name = "СЕМЕЙНАЯ ФОТОГРАФИЯ";
-                    $gallery_name = "СЕМЕЙНАЯ ФОТОГРАФИЯ";
-                    break;
-                case "fashion":
-                    $page_name =  "Фешн-съемка ";
-                    $gallery_name =  "Фешн-съемка ";
-                    break;
-                case "love":
-                    $page_name = "LOVE STORY - ФОТОГРАФИЯ";
-                    $gallery_name = "LOVE STORY - ФОТОГРАФИЯ";
-                    break;
-                case "wedding":
-                    $page_name = "Свадебная фотография";
-                    $gallery_name = "Свадебная фотография";
-                    break;
-                case "portrait":
-                    $page_name = " Портретная фотография";
-                    $gallery_name = " Портретная фотография";
-                    break;
-                case "nature":
-                    $page_name = " Съемка природы и ландшафты ";
-                    $gallery_name = " Съемка природы и ландшафты ";
-                    break;
-                case "food":
-                    $page_name = " PRODUCTION PHOTOGRAPHY ";
-                    $gallery_name = " PRODUCTION PHOTOGRAPHY ";
-                    break;
-                case "graduation":
-                    $page_name = " Фотосессия на выпускной  ";
-                    $gallery_name = "Фотосессия на выпускной  ";
-                    break;
-                case "reportage":
-                    $page_name = " Репортажная фотосъемка  ";
-                    $gallery_name = "Репортажная фотосъемка   ";
-                    break;
-            }
-        } elseif($_GET['lang']=='az'){
-            switch($foto){
-                case "family":
-                    $page_name = "Ailəvi çəkiliş. ";
-                    $gallery_name = "Ailəvi çəkiliş. ";
-                    break;
-                case "fashion":
-                    $page_name =  "Fashion çəkiliş. ";
-                    $gallery_name =  "Fashion çəkiliş. ";
-                    break;
-                case "love":
-                    $page_name = "LOVE STORY - ФОТОГРАФИЯ";
-                    $gallery_name = "LOVE STORY - ФОТОГРАФИЯ";
-                    break;
-                case "wedding":
-                    $page_name = "Свадебная фотография";
-                    $gallery_name = "Свадебная фотография";
-                    break;
-                case "portrait":
-                    $page_name = " Портретная фотография";
-                    $gallery_name = " Портретная фотография";
-                    break;
-                case "nature":
-                    $page_name = " Съемка природы и ландшафты ";
-                    $gallery_name = " Съемка природы и ландшафты ";
-                    break;
-                case "food":
-                    $page_name = " Food foto.  ";
-                    $gallery_name = " Food foto.  ";
-                    break;
-                case "graduation":
-                    $page_name = " Məzuniyyət fotosessiyası.  ";
-                    $gallery_name = "Məzuniyyət fotosessiyası.  ";
-                    break;
-                case "reportage":
-                    $page_name = " Репортажная фотосъемка  ";
-                    $gallery_name = "Репортажная фотосъемка   ";
-                    break;
-            }
-        }   
+if (isset($_GET['foto'])) {
+    $foto = $_GET['foto'];
+    if ($_GET['lang'] == 'ru') {
+        switch ($foto) {
+            case "family":
+                $page_name = "СЕМЕЙНАЯ ФОТОГРАФИЯ";
+                $gallery_name = "СЕМЕЙНАЯ ФОТОГРАФИЯ";
+                break;
+            case "fashion":
+                $page_name = "Фешн-съемка ";
+                $gallery_name = "Фешн-съемка ";
+                break;
+            case "love":
+                $page_name = "LOVE STORY - ФОТОГРАФИЯ";
+                $gallery_name = "LOVE STORY - ФОТОГРАФИЯ";
+                break;
+            case "wedding":
+                $page_name = "Свадебная фотография";
+                $gallery_name = "Свадебная фотография";
+                break;
+            case "portrait":
+                $page_name = " Портретная фотография";
+                $gallery_name = " Портретная фотография";
+                break;
+            case "nature":
+                $page_name = " Съемка природы и ландшафты ";
+                $gallery_name = " Съемка природы и ландшафты ";
+                break;
+            case "food":
+                $page_name = " PRODUCTION PHOTOGRAPHY ";
+                $gallery_name = " PRODUCTION PHOTOGRAPHY ";
+                break;
+            case "graduation":
+                $page_name = " Фотосессия на выпускной  ";
+                $gallery_name = "Фотосессия на выпускной  ";
+                break;
+            case "reportage":
+                $page_name = " Репортажная фотосъемка  ";
+                $gallery_name = "Репортажная фотосъемка   ";
+                break;
+        }
+    } elseif ($_GET['lang'] == 'az') {
+        switch ($foto) {
+            case "family":
+                $page_name = "Ailəvi çəkiliş. ";
+                $gallery_name = "Ailəvi çəkiliş. ";
+                break;
+            case "fashion":
+                $page_name = "Fashion çəkiliş. ";
+                $gallery_name = "Fashion çəkiliş. ";
+                break;
+            case "love":
+                $page_name = "LOVE STORY - ФОТОГРАФИЯ";
+                $gallery_name = "LOVE STORY - ФОТОГРАФИЯ";
+                break;
+            case "wedding":
+                $page_name = "Свадебная фотография";
+                $gallery_name = "Свадебная фотография";
+                break;
+            case "portrait":
+                $page_name = " Портретная фотография";
+                $gallery_name = " Портретная фотография";
+                break;
+            case "nature":
+                $page_name = " Съемка природы и ландшафты ";
+                $gallery_name = " Съемка природы и ландшафты ";
+                break;
+            case "food":
+                $page_name = " Food foto.  ";
+                $gallery_name = " Food foto.  ";
+                break;
+            case "graduation":
+                $page_name = " Məzuniyyət fotosessiyası.  ";
+                $gallery_name = "Məzuniyyət fotosessiyası.  ";
+                break;
+            case "reportage":
+                $page_name = " Репортажная фотосъемка  ";
+                $gallery_name = "Репортажная фотосъемка   ";
+                break;
+        }
     }
+}
 ?>
 
 
@@ -196,99 +196,62 @@ href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"
                 <div class="about__name__block">
                     <div class="gallery__main__name">
                         <?php
-                           echo $page_name;
+                        echo $page_name;
                         ?>
                     </div>
                 </div>
             </div>
                 <div class="gallery__content__container">
                     <div class="gallery__photos__wrapper">
-                        <a
-                          href="./imgs/content/1.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/1.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/2.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/2.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/3.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/3.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/4.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/4.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/5.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/5.jpg" alt="" srcset="" />
-                        </a>
-        
-                        <a
-                          href="./imgs/content/6.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/6.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                          href="./imgs/content/7.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/7.jpg" alt="" srcset="" />
-                        </a>
-        
-                        <a
-                          href="./imgs/content/8.jpg"
-                          data-fancybox="gallery"
-                          data-caption="Optional caption"
-                        >
-                          <img src="./imgs/content/8.jpg" alt="" srcset="" />
-                        </a>
-                        <a
-                        href="./imgs/content/9.jpg"
-                        data-fancybox="gallery"
-                        data-caption="Optional caption"
-                      >
-                        <img src="./imgs/content/9.jpg" alt="" srcset="" />
-                      </a>
-                      <a
-                      href="./imgs/content/10.jpg"
-                      data-fancybox="gallery"
-                      data-caption="Optional caption"
-                    >
-                      <img src="./imgs/content/10.jpg" alt="" srcset="" />
-                    </a>
-                    <a
-                    href="./imgs/content/11.jpg"
-                    data-fancybox="gallery"
-                    data-caption="Optional caption"
-                  >
-                    <img src="./imgs/content/11.jpg" alt="" srcset="" />
-                  </a>
-                  <a
-                  href="./imgs/content/12.jpg"
-                  data-fancybox="gallery"
-                  data-caption="Optional caption"
-                >
-                  <img src="./imgs/content/12.jpg" alt="" srcset="" />
-                </a>
+                        <?php
+                        switch ($foto) {
+                            case "family":
+                                for ($i = 1; $i < 10; $i++) {
+                                    ?>
+                                                        <a
+                                                          href="./imgs/family/<?php echo $i ?>.jpg"
+                                                          data-fancybox="gallery"
+                                                          data-caption="Optional caption"
+                                                        >
+                                                          <img src="./imgs/family/<?php echo $i ?>.jpg" alt="" srcset="" />
+                                                        </a>
+                                                                <?php
+                                }
+                                break;
+
+                            case "fashion":
+                                for ($i = 1; $i < 12; $i++) {
+                                    ?>
+                                                        <a
+                                                          href="./imgs/fashion/<?php echo $i ?>.jpg"
+                                                          data-fancybox="gallery"
+                                                          data-caption="Optional caption"
+                                                        >
+                                                          <img src="./imgs/fashion/<?php echo $i ?>.jpg" alt="" srcset="" />
+                                                        </a>
+                                                                <?php
+                                }
+                                break;
+
+
+
+                            case "love":
+                                for ($i = 1; $i < 15; $i++) {
+                                    ?>
+                                                            <a
+                                                              href="./imgs/love/<?php echo $i ?>.jpg"
+                                                              data-fancybox="gallery"
+                                                              data-caption="Optional caption"
+                                                            >
+                                                              <img src="./imgs/love/<?php echo $i ?>.jpg" alt="" srcset="" />
+                                                            </a>
+                                                                    <?php
+                                }
+                                break;
+                        }
+                        ?>
+
+
                       </div>
                 </div>
                     <!--------->  
